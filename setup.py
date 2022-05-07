dae954ab19b281c653d6e1329ea346637e350a05
Shahmatov Arseniy <62886550+cene555@users.noreply.github.com>
2022-05-07 20:39:30 +0300

    update2

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
         "dalle2_decoder.tokenizer",
     ],
     package_data={
-        "glide_text2im.tokenizer": [
+        "dalle2_decoder.tokenizer": [
             "bpe_simple_vocab_16e6.txt.gz",
             "encoder.json.gz",
             "vocab.bpe.gz",
         ],
-        "glide_text2im.clip": ["config.yaml"],
+        "dalle2_decoder.clip": ["config.yaml"],
     },
     install_requires=[
         "Pillow",
