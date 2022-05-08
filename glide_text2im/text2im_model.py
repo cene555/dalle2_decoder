36cd612611b7ca8a972e540206f9dca2ab45a5b2
Shahmatov Arseniy <62886550+cene555@users.noreply.github.com>
2022-05-08 12:46:27 +0300

    new_test5

--- glide_text2im/text2im_model.py
+++ glide_text2im/text2im_model.py
@@ -46,12 +46,76 @@ class Text2ImUNet(UNetModel):
             super().__init__(*args, **kwargs, encoder_channels=None)
         else:
             super().__init__(*args, **kwargs, encoder_channels=xf_width)
+        if self.xf_width:
+            self.transformer = Transformer(
+                text_ctx,
+                xf_width,
+                xf_layers,
+                xf_heads,
+            )
+            if xf_final_ln:
+                self.final_ln = LayerNorm(xf_width)
+            else:
+                self.final_ln = None
+
+            self.token_embedding = nn.Embedding(self.tokenizer.n_vocab, xf_width)
+            self.positional_embedding = nn.Parameter(th.empty(text_ctx, xf_width, dtype=th.float32))
+            self.transformer_proj = nn.Linear(xf_width, self.model_channels * 4)
+
+            if self.xf_padding:
+                self.padding_embedding = nn.Parameter(
+                    th.empty(text_ctx, xf_width, dtype=th.float32)
+                )
+            if self.xf_ar:
+                self.unemb = nn.Linear(xf_width, self.tokenizer.n_vocab)
+                if share_unemb:
+                    self.unemb.weight = self.token_embedding.weight
 
         self.cache_text_emb = cache_text_emb
         self.cache = None
 
     def convert_to_fp16(self):
         super().convert_to_fp16()
+        if self.xf_width:
+            self.transformer.apply(convert_module_to_f16)
+            self.transformer_proj.to(th.float16)
+            self.token_embedding.to(th.float16)
+            self.positional_embedding.to(th.float16)
+            if self.xf_padding:
+                self.padding_embedding.to(th.float16)
+            if self.xf_ar:
+                self.unemb.to(th.float16)
+
+    def get_text_emb(self, tokens, mask):
+        assert tokens is not None
+
+        if self.cache_text_emb and self.cache is not None:
+            assert (
+                tokens == self.cache["tokens"]
+            ).all(), f"Tokens {tokens.cpu().numpy().tolist()} do not match cache {self.cache['tokens'].cpu().numpy().tolist()}"
+            return self.cache
+
+        xf_in = self.token_embedding(tokens.long())
+        xf_in = xf_in + self.positional_embedding[None]
+        if self.xf_padding:
+            assert mask is not None
+            xf_in = th.where(mask[..., None], xf_in, self.padding_embedding[None])
+        xf_out = self.transformer(xf_in.to(self.dtype))
+        if self.final_ln is not None:
+            xf_out = self.final_ln(xf_out)
+        xf_proj = self.transformer_proj(xf_out[:, -1])
+        xf_out = xf_out.permute(0, 2, 1)  # NLC -> NCL
+
+        outputs = dict(xf_proj=xf_proj, xf_out=xf_out)
+
+        if self.cache_text_emb:
+            self.cache = dict(
+                tokens=tokens,
+                xf_proj=xf_proj.detach(),
+                xf_out=xf_out.detach() if xf_out is not None else None,
+            )
+
+        return outputs
 
     def del_cache(self):
         self.cache = None
@@ -59,8 +123,12 @@ class Text2ImUNet(UNetModel):
     def forward(self, x, timesteps, clip_emb=None, tokens=None, mask=None):
         hs = []
         emb = self.time_embed(timestep_embedding(timesteps, self.model_channels))
-        emb = emb + clip_emb.to(emb)
-        xf_out = None
+        if self.xf_width:
+            text_outputs = self.get_text_emb(tokens, mask)
+            xf_proj, xf_out = text_outputs["xf_proj"], text_outputs["xf_out"]
+            emb = emb + xf_proj.to(emb) + clip_emb.to(emb)
+        else:
+            xf_out = None
         h = x.type(self.dtype)
         for module in self.input_blocks:
             h = module(h, emb, xf_out)
