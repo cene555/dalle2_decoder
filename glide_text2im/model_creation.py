4bbd092648afc1218c9648716ae3c64e92b7f623
Shahmatov Arseniy <62886550+cene555@users.noreply.github.com>
2022-05-13 19:08:58 +0300

    Update model_creation.py

--- glide_text2im/model_creation.py
+++ glide_text2im/model_creation.py
@@ -1,13 +1,13 @@
 from glide_text2im.gaussian_diffusion import get_named_beta_schedule
 from . import gaussian_diffusion as gd
 from .respace import SpacedDiffusion, space_timesteps
-from glide_text2im.text2im_model import (
+from dalle2_decoder.text2im_model import (
     InpaintText2ImUNet,
     SuperResInpaintText2ImUnet,
     SuperResText2ImUNet,
     Text2ImUNet,
 )
-from glide_text2im.tokenizer.bpe import get_encoder
+from dalle2_decoder.tokenizer.bpe import get_encoder
 
 
 def model_and_diffusion_defaults():
